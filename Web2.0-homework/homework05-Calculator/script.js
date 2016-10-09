7984ec836529fc6d8159f6d056e6ae94b0a0955a
luguanxing <651571925@qq.com>
2016-10-09 16:16:44 +0800

    Calculator

--- Web2.0-homework/homework05-Calculator/script.js
+++ Web2.0-homework/homework05-Calculator/script.js
@@ -1,4 +1,4 @@
-MAX_INT = 9007199254740992;
+MAX_INT = 9007199254740992;		//防止运算过大的数导致不精确
 MIN_INT = -9007199254740992;
 
 function showstring(result) {
@@ -15,38 +15,6 @@ function backstring() {
 	var result = document.getElementById("screen").value;
 	result = result.substring(0, result.length-1);
 	document.getElementById("screen").value = result;
-}
-
-function isoperator(s) {
-	if (!isNaN(s))
-		return false;
-	if (s == "(" || s == ")")
-		return false;
-	return true;
-}
-
-function check() {
-	var string = document.getElementById("screen").value;
-	if (string == "")
-		return false;	
-	else if (isNaN(string[0]) && string[0] != "(" && string[0] != "-")
-		return false;
-	else if (isNaN(string[string.length-1]) && string[string.length-1] != ")")
-		return false;
-	else {
-		var leftbracket = 0, rightbracket = 0;
-		for (var i = 0; i < string.length; i++) {
-			if (i != 0 && isoperator(string[i]) && isoperator(string[i-1]))
-				return false;
-			if (string[i] == "(")
-				leftbracket++;
-			if (string[i] == ")")
-				rightbracket++;			
-		}
-		if (leftbracket != rightbracket)
-			return false;	
-	}
-	return true;
 }
 
 function setbutton() {
@@ -66,16 +34,11 @@ function setbutton() {
 	}
 	document.getElementById("=").onclick = function() {
 		try {
-			if (check()) {
-				var result = eval(document.getElementById("screen").value);
-				if (MIN_INT < result && result < MAX_INT)
-					showstring(parseFloat(result.toFixed(6)));
-				else {
-					alert("Overflow!");
-					showstring("");
-				}
-			} else {
-				alert("Error!");
+			var result = eval(document.getElementById("screen").value);
+			if (MIN_INT < result && result < MAX_INT)
+				showstring(parseFloat(result.toFixed(6)));
+			else {
+				alert("Overflow!");
 				showstring("");
 			}
 		} catch (error) {
