83b1de75578e7cbf936ff8b8d743b464f83bcc21
luguanxing <651571925@qq.com>
2017-08-14 00:48:00 +0800

    AngularJS

---
+++ AngularJS/基础教程/04-ngBind/js/index.js
@@ -0,0 +1,9 @@
+
+firstController = function ($scope) {
+	
+	// 在$scope作用域下声明model
+	$scope.name = "默认值";
+
+	$scope.age = 123;
+	
+}
---
+++ AngularJS/基础教程/05-多个控制器/js/index.js
@@ -0,0 +1,13 @@
+
+firstController = function ($scope) {
+	
+	$scope.name = "默认值1";
+	console.log($scope);
+	
+}
+
+secondController = function ($scope) {
+	
+	console.log($scope);
+	
+}
