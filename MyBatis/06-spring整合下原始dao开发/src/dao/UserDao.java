2d78888f31d97e3962694dacc351e9e7750195fc
luguanxing <651571925@qq.com>
2017-10-16 01:28:16 +0800

    spring整合下原始dao开发

---
+++ MyBatis/06-spring整合下原始dao开发/src/dao/UserDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import java.util.List;
+
+import pojo.User;
+
+public interface UserDao {
+	/**
+	 * 根据id查询用户
+	 * 
+	 * @param id
+	 * @return
+	 */
+	User queryUserById(int id);
+
+	/**
+	 * 根据用户名模糊查询用户列表
+	 * 
+	 * @param username
+	 * @return
+	 */
+	List<User> queryUserByUsername(String username);
+
+	/**
+	 * 保存
+	 * 
+	 * @param user
+	 */
+	void saveUser(User user);
+
+}
+
