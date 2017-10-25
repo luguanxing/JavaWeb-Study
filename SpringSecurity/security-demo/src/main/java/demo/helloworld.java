b6f564718445313e197191469d28967e6670bdae
luguanxing <651571925@qq.com>
2017-10-26 01:15:22 +0800

    环境配置和入门程序

---
+++ SpringSecurity/security-demo/src/main/java/demo/helloworld.java
@@ -0,0 +1,22 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class helloworld {
+
+	public static void main(String[] args) {
+		SpringApplication.run(helloworld.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello spring security";
+	}
+
+}
